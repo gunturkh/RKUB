32a8857bb8021ef7d639afd649bd862156e21f8c
GunturKH <guntur.kh@gmail.com>
2020-10-24 17:36:59 +0700

    Red logout button

--- components/Navbar.js
+++ components/Navbar.js
@@ -565,7 +565,7 @@ const NavBar = () => {
                         cookie.remove("user");
                         router.push("/login");
                       }}
-                      className="whitespace-no-wrap inline-flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
+                      className="whitespace-no-wrap inline-flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red active:bg-red-700 transition ease-in-out duration-150"
                     >
                       Log Out
                     </button>
