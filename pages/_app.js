62e4fd15412309626bd7718fb4fad497c5b07fbb
GunturKH <guntur.kh@gmail.com>
2020-10-24 16:57:54 +0700

    Set tailwind and change header layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
+import "../styles/index.css";
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <Layout>
-          <Component {...pageProps} />  
-        </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
 
+export default MyApp;
 
-  export default MyApp
